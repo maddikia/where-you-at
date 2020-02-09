72941fdd89573c22c97a7d9130b9964bc5982692
Pizzly-Bear <60847361+Pizzly-Bear@users.noreply.github.com>
2020-02-09 10:31:42 -0500

    Update main.js

---
+++ static/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $('#register').click(function() {
+        const username = $('#regusername').val();
+        const password = $('#regpassword').val();
+        const confirmPassword = $('#regconfirmpassword').val();
+        if (password != confirmPassword) {
+            return;
+        }
+        $.post('/register', {
+            username: username,
+            password: password,
+        }).done(function() {
+            document.location.reload();
+        });
+    })
+    $('#login').click(function() {
+        const username = $('#username').val();
+        $.post('/login', {
+            username: username,
+        }).done(function () {
+            document.location.reload();
+        });
+    });
+    $('#logout').click(function() {
+        $.post('/logout', {}).done(function() {
+            document.location.reload();
+        });
+    })
+
+    $('#submit').click(function () {
+        const message = $('#message').val();
+        $.post('/message', {
+            message: message,
+        }).done(function () {
+            document.location.reload();
+        });
+    });
+});
